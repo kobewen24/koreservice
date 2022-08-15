557937cfde285afc75cc8eeb38f009149f5cbcc7
kobewen24 <66712380+kobewen24@users.noreply.github.com>
2022-08-15 13:25:29 +0800

    Update KoreAIController.java

--- src/main/java/com/dialogflow/dialogflow/controller/KoreAIController.java
+++ src/main/java/com/dialogflow/dialogflow/controller/KoreAIController.java
@@ -21,6 +21,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,7 +33,7 @@ public class KoreAIController{
 	
 	private String inr_itempicker_doc = "{\"rLogId\":\"t6hheiwvpd1%28ciuc1mlnm2%3Asn%3D9ipthheiwvpd1%28%60%7F1au*w%60ut3457-1829fbc5556-0x2340\",\"success\":true,\"documentId\":[\"BOTC4013\"],\"preferredLanguageLocale\":\"en_US\",\"siteId\":0,\"contentDetails\":[{\"documentId\":\"BOTC4013\",\"localeId\":\"en_US\",\"documentType\":\"BOT_CONTENT\",\"cacheVersion\":1807,\"contentData\":[{\"name\":\"BOT_CONTENT.TITLE\",\"value\":\"inr.1-inr_entry - guest user\"},{\"name\":\"BOT_CONTENT.DISPLAY_TITLE\",\"value\":\"Item hasn't arrived\"},{\"name\":\"BOT_CONTENT.DESKTOP\",\"value\":\"<div>So I can best guide you, please select one of the options below:</div>\\n\\n<div class=\\\"button_group\\\">\\n<div>My order hasn&#39;t arrived</div>\\n\\n<div>Help a buyer with an item that hasn&#39;t arrived</div>\\n</div>\\n\"},{\"name\":\"BOT_CONTENT.MOBILE_WEB\",\"value\":\"<div>So I can best guide you, please select one of the options below:</div>\\n\\n<div class=\\\"button_group\\\">\\n<div>My order hasn&#39;t arrived</div>\\n\\n<div>Help a buyer with an item that hasn&#39;t arrived</div>\\n</div>\\n\"},{\"name\":\"BOT_CONTENT.TOPIC_ID\",\"value\":\"4042\"}],\"contentTags\":[]}]}";
 	
-	@PostMapping(value="/getContentDetail/{docId}/{siteId}")
+	@GetMapping(value="/getContentDetail/{docId}/{siteId}")
 	@ResponseBody
 	public ContentTemplateResponse endConversation(@PathVariable("docId") String docId, @PathVariable("siteId") String siteId) throws Exception {
 		ContentTemplateResponse response = new ObjectMapper().readValue(inr_itempicker_doc, ContentTemplateResponse.class);
